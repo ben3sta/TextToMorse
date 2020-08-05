359c4bf5831c3279fd74df7b4ec7611f3dbbfc67
ben3sta <69174432+ben3sta@users.noreply.github.com>
2020-08-05 10:23:18 -0400

    updated ttm.py
    Changed the way the sounds are found

--- ttm.py
+++ ttm.py
@@ -13,89 +13,120 @@ def line(x):
     for i in range(x):
         line_sound()
 
+def letterA():
+    dot(1)
+    line(1)
+def letterB():
+    line(1)
+    dot(3)
+def letterC():
+    line(1)
+    dot(1)
+    line(1)
+    dot(1)
+def letterD():
+    line(1)
+    dot(2)
+def letterE():
+    dot(1)
+def letterF():
+    dot(2)
+    line(1)
+    dot(1)
+def letterG():
+    line(2)
+    dot(1)
+def letterH():
+    dot(4)
+def letterI():
+    dot(2)
+def letterJ():
+    dot(1)
+    line(3)
+def letterK():
+    line(1)
+    dot(1)
+    line(1)
+def letterL():
+    dot(1)
+    line(1)
+    dot(2)
+def letterM():
+    line(2)
+def letterN():
+    line(1)
+    dot(1)
+def letterO():
+    line(3)
+def letterP():
+    dot(1)
+    line(2)
+    dot(1)
+def letterQ():
+    line(2)
+    dot(1)
+    line(1)
+def letterR():
+    dot(1)
+    line(1)
+    dot(1)
+def letterS():
+    dot(3)
+def letterT():
+    line(1)
+def letterU():
+    dot(2)
+    line(1)
+def letterV():
+    dot(3)
+    line(1)
+def letterW():
+    dot(1)
+    line(2)
+def letterX():
+    line(1)
+    dot(2)
+    line(1)
+def letterY():
+    line(1)
+    dot(1)
+    line(2)
+def letterZ():
+    line(2)
+    dot(2)
+
+
+dict = {"a": letterA,
+        "b": letterB,
+        "c": letterC,
+        "d": letterD,
+        "e": letterE,
+        "f": letterF,
+        "g": letterG,
+        "h": letterH,
+        "i": letterI,
+        "j": letterJ,
+        "k": letterK,
+        "l": letterL,
+        "m": letterM,
+        "n": letterN,
+        "o": letterO,
+        "p": letterP,
+        "k": letterK,
+        "r": letterR,
+        "s": letterS,
+        "t": letterT,
+        "u": letterU,
+        "v": letterV,
+        "w": letterW,
+        "x": letterX,
+        "y": letterY,
+        "z": letterZ,
+    }
+
 def morse(text):
     for x in text:
-        if x == "a":
-            dot(1)
-            line(1)
-        elif x == "b":
-            line(1)
-            dot(3)
-        elif x == "c":
-            line(1)
-            dot(1)
-            line(1)
-            dot(1)
-        elif x == "d":
-            line(1)
-            dot(2)
-        elif x == "e":
-            dot(1)
-        elif x == "f":
-            dot(2)
-            line(1)
-            dot(1)
-        elif x == "g":
-            line(2)
-            dot(1)
-        elif x == "h":
-            dot(4)
-        elif x == "i":
-            dot(2)
-        elif x == "j":
-            dot(1)
-            line(3)
-        elif x == "k":
-            line(1)
-            dot(1)
-            line(1)
-        elif x == "l":
-            dot(1)
-            line(1)
-            dot(2)
-        elif x == "m":
-            line(2)
-        elif x == "n":
-            line(1)
-            dot(1)
-        elif x == "o":
-            line(3)
-        elif x == "p":
-            dot(1)
-            line(2)
-            dot(1)
-        elif x == "q":
-            line(2)
-            dot(1)
-            line(1)
-        elif x == "r":
-            dot(1)
-            line(1)
-            dot(1)
-        elif x == "s":
-            dot(3)
-        elif x == "t":
-            line(1)
-        elif x == "u":
-            dot(2)
-            line(1)
-        elif x == "v":
-            dot(3)
-            line(1)
-        elif x == "w":
-            dot(1)
-            line(2)
-        elif x == "x":
-            line(1)
-            dot(2)
-            line(1)
-        elif x == "y":
-            line(1)
-            dot(1)
-            line(2)
-        elif x == "z":
-            line(2)
-            dot(2)
+        dict[x]()
         time.sleep(0.5)
 
 welcome = pyfiglet.figlet_format("Text To Morse")
